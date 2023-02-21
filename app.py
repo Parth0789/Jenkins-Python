d54ad5a1e273e8cafcf905af5ffd6344b8c5adae
Parth0789 <parthmanchanda0789@gmail.com>
2023-02-21 15:52:32 +0530

    add a new endpoint for jenkins testing

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return {"message": "Hello World!!"}, 200
 
+@app.get("/todo")
+def todo():
+    return {"message": "Todo app Successfully running"}, 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
